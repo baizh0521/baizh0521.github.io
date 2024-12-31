807d8e66c78f28c515ad7554e19804c2fac4f698
baizh0521 <baizh0521@163.com>
2024-12-31 23:59:31 +0800

    Site updated: 2024-12-31 23:59:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/30/hello-world/"];function toRandomPost(){
+var posts=["wenzhang/元旦快乐/","wenzhang/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
